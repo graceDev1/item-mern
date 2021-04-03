80f0ed73bf5dd025563e4afc2f8c17be0f5a704b
graceDev1 <grace.devolop@gmail.com>
2021-04-03 17:23:55 +0200

    setup context api

--- client/src/components/itemList/ItemList.component.jsx
+++ client/src/components/itemList/ItemList.component.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ContextProvider } from '../context/GlobalContext';
 
 function ItemList() {
+    const value = useContext(ContextProvider);
+    console.log(value);
     return (
         <section>
-            ItemList
+            <h1>ItemList {value}</h1> 
         </section>
     )
 }
